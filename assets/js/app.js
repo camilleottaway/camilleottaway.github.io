fdf749b909a1ef1738227adcb6920d55ad155d1c
Camille <camilleottaway@gmail.com>
2023-04-06 16:25:03 -0700

    debug

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,16 +12,18 @@ scrollUp.addEventListener("click", () => {
 // Nav hamburgerburger selections
 
 const burger_index = document.querySelector("#burger-menu");
-const burger_project = document.querySelector("#burger-menu-project");
+const burger_project = document.querySelector("#burger-menu-projects");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
+burger_index.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+  
 burger_project.addEventListener("click", () => {
     ul.classList.toggle("show");
   });
 
-  burger_index.addEventListener("click", () => {
-    ul.classList.toggle("show");
-  });
+  
 
   
